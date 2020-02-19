6b4264d3c07894ad0d1e7fb0ec1007c7465d1bd3
Stephen Vilim <svilim@drwholdings.com>
2020-02-19 15:07:30 -0600

    initial dotfiles commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+files=".vimrc .zshrc .config/i3/config .config/Code/User/settings.json .config/kitty/kitty.conf .Xresources .oh-my-zsh/themes/oxide.zsh-theme .config/kitty/theme.conf .config/i3blocks/config"
+
+for file in $files; do
+	ln -sf ~/dev/dotfiles/$file ~/$file
+done
